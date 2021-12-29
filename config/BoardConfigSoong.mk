de2b0672071707dbdacfd406816ca4f4771e0f6a
chaptsand <chaptsand@gmail.com>
2021-12-29 10:36:12 +0000

    vendor: Add udfps soong var
    Change-Id: I28c139d6e5e1878b57d1aff4531536d63652d58c

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,6 +32,7 @@ SOONG_CONFIG_fuseGlobalVars += \
     gralloc_handle_has_reserved_size \
     supports_extended_compress_format \
     target_init_vendor_lib \
+    target_surfaceflinger_udfps_lib \
     uses_qti_camera_device
 
 SOONG_CONFIG_NAMESPACES += fuseQcomVars
@@ -47,6 +48,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_fuseGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_fuseGlobalVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
+SOONG_CONFIG_fuseGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 SOONG_CONFIG_fuseGlobalVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 SOONG_CONFIG_fuseQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
@@ -54,6 +56,7 @@ SOONG_CONFIG_fuseQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
 TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_fuseGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
